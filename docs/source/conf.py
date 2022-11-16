0ca07b1022f77bc3a11462807103a1d9df7eb763
Imbrea Bogdan-Andrei <46221053+bogdanimbrea@users.noreply.github.com>
2022-11-16 23:36:51 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Discounting Cash Flows'
+copyright = '2022, discountingcashflows.com'
+author = 'Discounting Cash Flows'
 
 release = '0.1'
 version = '0.1.0'
